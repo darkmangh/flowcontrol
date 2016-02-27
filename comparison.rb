e53d8cbcc08f3db667e6bf4fc1e7bf61354324b5
darkmangh <charles_rex@yahoo.com>
2016-02-27 19:07:39 +0200

    comparison

---
+++ comparison.rb
@@ -0,0 +1,19 @@
+puts "ENTER ANY NUMBER :"
+
+number = gets.chomp.to_i
+
+if number < 0
+  puts "You cant enter a negative number"
+
+elsif number >= 0 && number <= 50
+  puts "#{number} is between 0 and 50"
+
+elsif number >= 51 && number <= 100
+
+  puts "#{number} is between 51 and 100"
+
+else
+
+ puts "#{number} is greater than 100"
+
+end
